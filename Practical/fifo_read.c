7080125e375abd0e2315e9406f7346e9f4784f10
urohitsai-264 <urohitsai@gmail.com>
2026-08-27 12:36:04 +0530

    Add files via upload

---
+++ Practical/fifo_read.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main(){
+int fd;
+char buffer[100];
+fd = open("myfifo", O_RDONLY);
+read(fd, buffer, sizeof(buffer));
+printf("Received: %s\n", buffer);
+close(fd);
+return 0;
+}
